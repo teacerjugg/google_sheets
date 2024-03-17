1200b04bfc9d04d6b365f8a9bc9972961beb2264
teacerjugg <github.rarity971@passmail.net>
2024-03-17 16:16:52 +0900

    Add comment

--- src/api/response.rs
+++ src/api/response.rs
@@ -1,6 +1,7 @@
 use serde::Deserialize;
 use serde_json::Value;
 
+/// https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/clear?hl=ja#response-body
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ClearResponse {
