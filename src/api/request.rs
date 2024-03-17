d42aec0cf33bcea10f4b49fd74e0ddca43e76681
teacerjugg <github.rarity971@passmail.net>
2024-03-17 17:00:36 +0900

    Add `Dimension`

--- src/api/request.rs
+++ src/api/request.rs
@@ -1,11 +1,20 @@
 use serde::Deserialize;
 use serde_json::Value;
 
+/// https://developers.google.com/sheets/api/reference/rest/v4/Dimension?hl=ja
+#[allow(non_camel_case_types)]
+#[derive(Debug, Deserialize)]
+pub enum Dimension {
+    DIMENSION_UNSPECIFIED,
+    ROWS,
+    COLUMNS,
+}
+
 /// https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values?hl=ja#ValueRange
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ValueRange {
     pub range: String,
-    pub major_dimension: String,
+    pub major_dimension: Dimension,
     pub values: Vec<Vec<Value>>,
 }
