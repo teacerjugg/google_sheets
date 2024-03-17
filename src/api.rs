36a388e5b267ed88a6ceddae7aed11c6ced8c6dc
teacerjugg <github.rarity971@passmail.net>
2024-03-17 15:01:25 +0900

    Update to separate module

--- src/api.rs
+++ src/api.rs
@@ -2,45 +2,12 @@ use anyhow::Result;
 use reqwest::Client;
 use serde_json::Value;
 
-/// https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption?hl=ja
-#[allow(non_camel_case_types)]
-pub enum ValueInputOption {
-    INPUT_VALUE_OPTION_UNSPECIFIED,
-    RAW,
-    USER_ENTERED,
-}
-
-impl std::fmt::Display for ValueInputOption {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            ValueInputOption::INPUT_VALUE_OPTION_UNSPECIFIED => {
-                write!(f, "INPUT_VALUE_OPTION_UNSPECIFIED")
-            }
-            ValueInputOption::RAW => write!(f, "RAW"),
-            ValueInputOption::USER_ENTERED => write!(f, "USER_ENTERED"),
-        }
-    }
-}
-
-/// https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append?hl=ja#InsertDataOption
-#[allow(non_camel_case_types)]
-pub enum InsertDataOption {
-    OVERWRITE,
-    INSERT_ROWS,
-}
-
-impl std::fmt::Display for InsertDataOption {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            InsertDataOption::OVERWRITE => write!(f, "OVERWRITE"),
-            InsertDataOption::INSERT_ROWS => write!(f, "INSERT_ROWS"),
-        }
-    }
-}
+mod query;
+pub use query::*;
 
 pub struct GoogleSheets {
-    spreadsheet_id: String,
-    access_token: String,
+    pub spreadsheet_id: String,
+    pub access_token: String,
 }
 
 impl GoogleSheets {
@@ -100,7 +67,7 @@ impl GoogleSheets {
         let response = client
             .post(&format!(
                 "https://sheets.googleapis.com/v4/spreadsheets/{}/values/{}:clear",
-                self.spreadsheet_id, range,
+                self.spreadsheet_id, range.as_ref(),
             ))
             .bearer_auth(&self.access_token)
             .send()
