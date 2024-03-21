78b8999efa5ad8720608c64e3f7cb1e59a8f457f
teacerjugg <github.rarity971@passmail.net>
2024-03-21 14:42:43 +0900

    Fix errors

--- src/api/util.rs
+++ src/api/util.rs
@@ -1,48 +1,39 @@
 use crate::param::Dimension;
 use serde_json::{json, Value};
-use std::convert::TryInto::Error;
 
-pub fn create_delete_duplicates_request<T>(
-    sheet_id: T,
-    start_index: T,
-    end_index: T,
-) -> Result<Value, Error>
+pub fn create_delete_duplicates_request<T>(sheet_id: T, start_index: T, end_index: T) -> Value
 where
-    T: TryInto<u32>,
+    T: Into<u32> + Copy,
 {
     json!({
         "deleteDuplicates": {
             "range": {
-                "sheetId": sheet_id.try_into()?,
+                "sheetId": sheet_id.into()
             },
             "comparisonColumns": [
                 {
                     "sheetId": sheet_id.into(),
                     "dimension": Dimension::COLUMNS,
-                    "startIndex": start_index.try_into()?,
-                    "endIndex": end_index.try_into()?,
+                    "startIndex": start_index.into(),
+                    "endIndex": end_index.into(),
                 }
             ],
         }
     })
 }
 
-pub fn create_sort_range_request<T>(
-    sheet_id: T,
-    column_index: T,
-    sort_order: Dimension,
-) -> Result<Value, Error>
+pub fn create_sort_range_request<T>(sheet_id: T, column_index: T, sort_order: Dimension) -> Value
 where
-    T: TryInto<u32>,
+    T: Into<u32> + Copy,
 {
     json!({
         "sortRange": {
             "range": {
-                "sheetId": sheet_id.try_into()?,
+                "sheetId": sheet_id.into(),
             },
             "sortSpecs": [
                 {
-                    "dimensionIndex": column_index.try_into()?,
+                    "dimensionIndex": column_index.into(),
                     "sortOrder": sort_order,
                 },
             ],
--- src/param.rs
+++ src/param.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 /// https://developers.google.com/sheets/api/reference/rest/v4/Dimension?hl=ja
 #[derive(Debug, Deserialize, Serialize)]
 #[allow(non_camel_case_types)]
